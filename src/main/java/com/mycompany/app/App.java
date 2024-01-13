e04ef66c8db51b394ca36c1c89fa000713361529
Adrian Niedbalski <niedbalskiadrian@gmail.com>
2024-01-13 11:42:08 +0100

    Changed again

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -38,9 +38,9 @@ public class App extends Application {
     public void start(Stage stage) {
 
         // set the title and size of the stage and show it
-        stage.setTitle("SDA Project 3");
-        stage.setWidth(2048);
-        stage.setHeight(1024);
+        stage.setTitle("123123");
+        stage.setWidth(1234);
+        stage.setHeight(1234);
         stage.show();
 
         // create a JavaFX scene with a stack pane as the root node and add it to the scene
