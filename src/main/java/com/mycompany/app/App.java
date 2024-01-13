a59b191d671ec66c09353d76ec5fa222d5a8d25c
Adrian Niedbalski <niedbalskiadrian@gmail.com>
2024-01-13 10:58:34 +0100

    Small changes in copyright

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019 Esri
+ * Copyright 2024 Adrian Niedbalski
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy
@@ -38,17 +38,18 @@ public class App extends Application {
     public void start(Stage stage) {
 
         // set the title and size of the stage and show it
-        stage.setTitle("My Map App");
+        stage.setTitle("SDA Project");
         stage.setWidth(1366);
         stage.setHeight(768);
         stage.show();
+        stage.close();
 
         // create a JavaFX scene with a stack pane as the root node and add it to the scene
         StackPane stackPane = new StackPane();
         Scene scene = new Scene(stackPane);
         stage.setScene(scene);
 
-        // Note: it is not best practice to store API keys in source code.
+
         // An API key is required to enable access to services, web maps, and web scenes hosted in ArcGIS Online.
         // If you haven't already, go to your developer dashboard to get your API key.
         // Please refer to https://developers.arcgis.com/java/get-started/ for more information
